24cd6a93abc438c1222643d288f2e58307b21e7a
Ajewole-CODER <ajewolelemuel@gmail.com>
2025-02-05 02:22:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,36 @@ document.getElementById('searchBtn').addEventListener('click', function() {
         alert('Please enter a search term.');
     }
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const links = dropdownMenu.querySelectorAll('a');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme + '-mode');
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-mode');
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.body.classList.remove('light-mode', 'dark-mode');
+
+            const theme = link.getAttribute('data-theme');
+
+            if (theme === 'system') {
+                localStorage.removeItem('theme');
+                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    document.body.classList.add('dark-mode');
+                }
+            } else {
+                document.body.classList.add(theme + '-mode');
+                localStorage.setItem('theme', theme);
+            }
+        });
+    });
+});
+
